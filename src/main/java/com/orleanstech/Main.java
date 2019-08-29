06a52b50d95ca1bb1ef699d1e2199cc357efbb0e
Alexandre Touret <alexandre.touret@free.fr>
2019-08-29 15:48:22 +0200

    Ajout Fichiers standards
    Signed-off-by: Alexandre Touret <alexandre.touret@free.fr>

---
+++ src/main/java/com/orleanstech/Main.java
@@ -0,0 +1,8 @@
+package com.orleanstech;
+
+public class Main {
+    public static void main(String[] args) {
+        MyPipeline myPipeline = new MyPipeline();
+        myPipeline.run(args);
+    }
+}
---
+++ src/main/java/com/orleanstech/MyPipeline.java
@@ -0,0 +1,9 @@
+package com.orleanstech;
+
+import java.io.Serializable;
+
+public class MyPipeline implements Serializable {
+    public void run(String[] args) {
+
+    }
+}
