7df7efec9031dc512b94781233428f4b66f42b92
Alexandre Touret <alexandre.touret@free.fr>
2019-08-30 14:49:50 +0200

    Debut projet Creation template
    Signed-off-by: Alexandre Touret <alexandre.touret@free.fr>

--- src/main/java/com/orleanstech/MyPipeline.java
+++ src/main/java/com/orleanstech/MyPipeline.java
@@ -1,9 +1,13 @@
 package com.orleanstech;
 
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+
 import java.io.Serializable;
 
 public class MyPipeline implements Serializable {
     public void run(String[] args) {
+        Pipeline pipeline = Pipeline.create(PipelineOptionsFactory.fromArgs(args).withValidation().create());
 
     }
 }
