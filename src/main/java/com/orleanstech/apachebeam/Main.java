72973f722b0ceef5e6472ebf9f908742bbe9df88
Alexandre Touret <alexandre.touret@free.fr>
2019-08-30 15:38:01 +0200

    Debut projet Creation template
    Signed-off-by: Alexandre Touret <alexandre.touret@free.fr>

--- src/main/java/com/orleanstech/apachebeam/Main.java
+++ src/main/java/com/orleanstech/apachebeam/Main.java
@@ -1,4 +1,4 @@
-package com.orleanstech;
+package com.orleanstech.apachebeam;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/com/orleanstech/apachebeam/MyPipeline.java
+++ src/main/java/com/orleanstech/apachebeam/MyPipeline.java
@@ -1,13 +1,19 @@
-package com.orleanstech;
+package com.orleanstech.apachebeam;
 
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 
 import java.io.Serializable;
 
 public class MyPipeline implements Serializable {
+    public MyPipeline() {
+    }
+
     public void run(String[] args) {
         Pipeline pipeline = Pipeline.create(PipelineOptionsFactory.fromArgs(args).withValidation().create());
-
+        pipeline.apply(TextIO.read().from("data/*.csv"))
+                .apply(TextIO.write().to("target/orleanstech"));
+        pipeline.run();
     }
 }
